b70d1227953b66203cba2d8a5912dfc6d89bdb3e
Sediq <sediqullah.kismatkhan@powercoders.org>
2024-04-26 11:35:36 +0200

    Profiles Data

---
+++ script.js
@@ -0,0 +1,84 @@
+const profArr = [
+{
+    name: "Sediq",
+    age: 29,
+    itSkills: ["HTML", "CSS", "C++"],
+    hobby: ["Football", "Reading"]
+},
+{
+    name: "Kashif",
+    age: 20,
+    itSkills: ["Javascript", "CSS", "HTML"],
+    hobby: ["Coding, Playing board games"],
+},
+
+{
+    name: "Yurii",
+    age: 40,
+    itSkills: ["Typescript", "CSS", "HTML"],
+    hobby: ["Reading", "Medel Building"]
+},
+
+{
+    name: "Tugba",
+    age: 38,
+    itSkills: ["Javascript", "React.js", "HTML"],
+    hobby: ["Hiking", "New Language"]
+},
+
+{
+    name: "Juan",
+    age: 36,
+    itSkills: ["Node.js", "CSS", "HTML"],
+    hobby: ["Snowboarding", "Fishing"]
+},
+
+{
+    name: "Shakir",
+    age: 24,
+    itSkills: ["Javascript", "CSS", "HTML"],
+    hobby: ["Tennis", "Maritial Arts"]
+},
+
+{
+    name: "Enes",
+    age: 36,
+    itSkills: ["Javascript", "Python", "HTML"],
+    hobby: ["Painting", "Basketball"]
+},
+
+{
+    name: "Frank",
+    age: 33,
+    itSkills: ["Algorithms", "CSS", "HTML"],
+    hobby: ["Writting", "Astronomy"]
+},
+
+{
+    name: "Olena",
+    age: 32,
+    itSkills: ["Javascript", "React Native", "HTML"],
+    hobby: ["Cooking", "Drawing"]
+},
+
+{
+    name: "Nizami",
+    age: 34,
+    itSkills: ["Javascript", "CSS", "HTML"],
+    hobby: ["Photgraphy", "Poetry"]
+},
+
+{
+    name: "Violett",
+    age: 23,
+    itSkills: ["Testing", "CSS", "HTML"],
+    hobby: ["Playing piano", "Knitting"]
+},
+
+{
+    name: "Olga",
+    age: 30,
+    itSkills: ["Javascript", "CSS", "HTML"],
+    hobby: ["Cooking", "Volunteering"]
+}
+]
